cdc9d228d1a8fa542d82a7faea4960d7c8c90f79
Noah Gilchrist <ngilchrist815@gmail.com>
2023-11-24 18:34:03 -0500

    Added in the ESP Code as well

---
+++ DistanceFindingModule/BleScanPrototype/ESPBluetooth/ESPBluetooth.ino
@@ -0,0 +1,157 @@
+/*
+   Based on Neil Kolban example for IDF: https://github.com/nkolban/esp32-snippets/blob/master/cpp_utils/tests/BLE%20Tests/SampleScan.cpp
+   Ported to Arduino ESP32 by Evandro Copercini
+*/
+
+#include <BLEDevice.h>
+#include <BLEUtils.h>
+#include <BLEScan.h>
+#include <BLEAdvertisedDevice.h>
+
+uint8_t mac[] = { 0x47, 0x32, 0xee, 0x1f, 0xc6, 0x85 };
+int scanTime = 5; //In seconds
+BLEScan* pBLEScan;
+bool isPaused;
+byte i;
+byte total;
+byte limit;
+string connectedName;
+//change string name possibility 
+int rssi[] = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+class MyAdvertisedDeviceCallbacks: public BLEAdvertisedDeviceCallbacks {
+    void onResult(BLEAdvertisedDevice advertisedDevice) 
+    {
+      //add in a parse to a specific UID 
+      BLEAddress address = advertisedDevice.getAddress();
+      //this command will find the specific UUID advertisedDevice.getServiceUUID();
+
+
+      uint8_t* macAddr = (uint8_t*)address.getNative();
+      // if(advertisedDevice.getName().c_str()[0] == 'P' &&
+      //    advertisedDevice.getName().c_str()[1] == 'i' &&
+      //    advertisedDevice.getName().c_str()[2] == 'x' &&
+      //    advertisedDevice.getName().c_str()[3] == 'e' &&
+      //    advertisedDevice.getName().c_str()[4] == 'l' &&
+      //    advertisedDevice.getName().c_str()[5] == ' ' &&
+      //    advertisedDevice.getName().c_str()[6] == '3')
+        if(advertisedDevice.getAddress() == connectedName)
+        //find the RSSI and place here
+      {
+        //commented out to avoid issues
+        //Serial.printf("%i\n", advertisedDevice.getRSSI());
+        rssi[i] = advertisedDevice.getRSSI();
+        i++;
+        i = i % 16;
+        total++;
+      }
+    }
+};
+
+void setup() {
+  Serial.begin(9600);
+  //Stream.begin(9600);
+  Serial.println("Scanning...");
+  isPaused = true;
+  i = 0;
+  total = 0;
+  connectedName = "Pixel 3";
+  BLEDevice::init("");
+  pBLEScan = BLEDevice::getScan(); //create new scan
+  pBLEScan->setAdvertisedDeviceCallbacks(new MyAdvertisedDeviceCallbacks());
+  pBLEScan->setActiveScan(true); //active scan uses more power, but get results faster
+  pBLEScan->setInterval(200);
+  pBLEScan->setWindow(99);  // less or equal setInterval value
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  if(isPaused)
+  {
+    delay(250);
+    if(Serial.available() > 0)
+    {
+
+      // Pause.
+      //isPaused = false;
+      while(Serial.available() > 0)
+      {
+        String command = Serial.readStringUntil('\n');
+        Serial.print(command);
+        if(command.compareTo("ID") == 0)
+        {
+          // while(Serial.available()> 0 )
+          // {
+          //   Serial.read();
+          // }
+          
+
+          Serial.write("\nTwo\n");
+          continue;
+        }
+        else if(command.compareTo("DISTANCE") == 0)
+        {
+          isPaused = false;
+          limit = 6;
+          continue;
+        }
+        else if(command.compareTo("CALIBRATE")==0)
+        {
+          isPaused = false;
+          limit = 16;
+          continue;
+          
+        }
+        else if(command.indexOf("NAME") > 0)
+        {
+          connectedName = command.substring(command.indexOf(('E'),command.length()-1));
+          Serial.print("\n");//Might not be needed
+          Serial.flush();
+        }
+        else
+        {
+           Serial.print("\nError no command\n");
+          Serial.flush();
+        }
+       
+      }
+
+      //Serial.println("Started");
+    }
+  }
+  else
+  {
+    if(total >= limit)
+    {
+      // Pause.
+      isPaused = true;
+
+      // Get average.
+      Serial.printf("\n%3.2f\n", (rssi[0] + rssi[1] + rssi[2] + rssi[3]) / 4);
+      rssi[0] = 0;
+      rssi[1] = 0;
+      rssi[2] = 0;
+      rssi[3] = 0;
+      rssi[4] = 0;
+      rssi[5] = 0;
+      rssi[6] = 0;
+      rssi[7] = 0;
+      rssi[8] = 0;
+      rssi[9] = 0;
+      rssi[10] = 0;
+      rssi[11] = 0;
+      rssi[12] = 0;
+      rssi[13] = 0;
+      rssi[14] = 0;
+      rssi[15] = 0;
+      i = 0;
+      total = 0;
+      //Serial.println("Paused");
+    }
+    else
+    {
+      delay(2000);
+      BLEScanResults foundDevices = pBLEScan->start(scanTime, false);
+      pBLEScan->clearResults();   // delete results fromBLEScan buffer to release memory
+    }
+  }
+}
